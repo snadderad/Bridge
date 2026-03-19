07878d1bf1f2c4c70f783130d079476ae41088ed
snadderad <sanderrtin@gmail.com>
2026-03-19 22:25:40 +0100

    modified:   server.py

--- server.py
+++ server.py
@@ -2,17 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# fake database
 users = [
     {"username": "admin", "password": "1234"} #Admin user
 ]
 
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
+    data = request.get_json(force=True)
 
     username = data.get("username")
     password = data.get("password")
+
+    print(request.data)
+
+    print(f"Received login attempt for username: {username}"
+          f" with password: {password}")
 
     for user in users:
         if user["username"] == username and user["password"] == password:
