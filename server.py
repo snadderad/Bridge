470a66fe700052cae30ddfdc63b17f5c5280514c
snadderad <sanderrtin@gmail.com>
2026-03-21 10:55:10 +0100

    new file:   .gitignore 	modified:   server.py

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+ 
 
 app = Flask(__name__)
+
 
 users = [
     {"username": "admin", "password": "1234"} #Admin user
 ]
+
+@app.route("/")
+def home():
+        print("Website active")
+        return send_from_directory("static", "index.html")
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -13,10 +21,15 @@ def login():
     username = data.get("username")
     password = data.get("password")
 
+    print("Received login request:")
+
     print(request.data)
 
     print(f"Received login attempt for username: {username}"
           f" with password: {password}")
+    
+    
+        
 
     for user in users:
         if user["username"] == username and user["password"] == password:
